791efe6b19c2b65a0c4eac038408c5802e264007
Alex Nichol <alexnichol@comcast.net>
2011-08-15 16:00:47 -0400

    Initial Commit

---
+++ PDFCrack/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  PDFCrack
+//
+//  Created by Alex Nichol on 8/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
